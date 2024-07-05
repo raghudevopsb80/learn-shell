999460fd088a98270554f8d2df5f21a7089aa1b3
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-05 06:08:45 +0530

    Functions

---
+++ 02-functions.sh
@@ -0,0 +1,26 @@
+# Function is a name given to a set of commands.
+# Syntax
+
+# function abc() {
+# commands
+# commands
+# }
+
+# OR
+
+# abc() {
+# commands
+# commands
+#}
+
+# How to call a function. - Simply call the function like a command
+
+# abc
+
+
+SAMPLE() {
+  echo SAMPLE FUNCTION
+}
+
+SAMPLE
+SAMPLE
