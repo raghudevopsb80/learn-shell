9279284e1834cdd838d00dfd440862f52053c9de
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-08 06:13:51 +0530

    Shell script

---
+++ 05-quotes.sh
@@ -0,0 +1,9 @@
+# Shell Supports two quotes
+# ' - single quote
+# " - Double quote
+
+# When to use quotes
+# if our input to any command is having a special characters ( not a-z, not 0-9, not _ ), then to nullify those speciality we use quotes
+
+echo *
+echo "*"
